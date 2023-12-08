6205df8d7674d020d000d9bc4a2f0aa8fabdbd76
aimhighdream <suzetteoeder@gmail.com>
2023-12-08 05:29:05 -0800

    updated successfully

--- server/services/processPost.js
+++ server/services/processPost.js
@@ -21,49 +21,91 @@ const processPost = async (req, res, next) => {
       content,
       timeout
     );
-
+ 
     if (Object.keys(categories).length > 0) {
+
+      
       const recommendedCommunity = Object.keys(categories)[0];
 
+
+      
       if (recommendedCommunity !== communityName) {
+      
         const type = "categoryMismatch";
+      
         const info = {
+      
           community: communityName,
+      
           recommendedCommunity,
+      
         };
 
+
+        
         return res.status(403).json({ type, info });
-      } else {
+      } 
+      else {
+      
         req.failedDetection = false;
+      
         next();
+      
       }
-    } else {
+    } 
+    else {
+    
       req.failedDetection = true;
+    
       next();
+    
     }
-  } catch (error) {
+  } 
+  catch (error) {
+  
     const errorMessage = `Error processing post: ${error.message}`;
+  
     await saveLogInfo(null, errorMessage, serviceProvider, "error");
+  
     return res.status(500).json({ message: "Error processing post" });
+  
   }
 };
 
+
+
+
 const getSystemPreferences = async () => {
+
   try {
+
     const config = await Config.findOne({}, { _id: 0, __v: 0 });
 
+
+    
     if (!config) {
+    
       return {
+    
         serviceProvider: "disabled",
+    
         timeout: 10000,
+    
       };
+    
     }
 
+
+    
     const {
+    
       categoryFilteringServiceProvider: serviceProvider = "disabled",
+    
       categoryFilteringRequestTimeout: timeout = 10000,
+    
     } = config;
 
+    
     return {
       serviceProvider,
       timeout,
